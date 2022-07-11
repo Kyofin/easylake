53b612f586c9bcd0194a3fbcb3cb00abec550f7c
huzekang <1040080742@qq.com>
2022-07-11 14:29:55 +0800

    init

---
+++ src/main/java/io/lake/easylake/EasylakeApplication.java
@@ -0,0 +1,13 @@
+package io.lake.easylake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasylakeApplication.class, args);
+	}
+
+}
