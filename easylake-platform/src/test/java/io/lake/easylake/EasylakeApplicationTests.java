1c22f6def5d9d22fd65460be63a508e8c10a0d44
huzekang <1040080742@qq.com>
2022-07-18 16:59:27 +0800

    增加Iceberg读

--- easylake-platform/src/test/java/io/lake/easylake/EasylakeApplicationTests.java
+++ easylake-platform/src/test/java/io/lake/easylake/EasylakeApplicationTests.java
@@ -300,36 +300,39 @@ class EasylakeApplicationTests {
 		Configuration conf = new Configuration();
 		HadoopTables tables = new HadoopTables(conf);
 		// 不指定namespace和表名，直接指定路径
-		final String tableLocation = "file:///Volumes/Samsung_T5/opensource/easylake/data/iceberg_warehouse/iceberg_warehouse/tb5";
+		final String tableLocation = "file:///Volumes/Samsung_T5/opensource/easylake/data/iceberg_warehouse/iceberg_warehouse/tb3";
 		final Table table = tables.load(tableLocation);
+
+		table.history().forEach(historyEntry -> {
+			System.out.println("快照id:"+historyEntry.snapshotId());
+		});
+
 		table.snapshots().forEach(snapshot -> {
-			System.out.println("--------------" + snapshot.snapshotId());
+			System.out.println("--------------快照概要：" + snapshot.snapshotId());
 			snapshot.summary().forEach((k, v) -> {
 				System.out.println(k + ":" + v);
 			});
 		});
-		table.history().forEach(historyEntry -> {
-			System.out.println(historyEntry.snapshotId());
-		});
+
 
 		// 根据数据找文件
 		TableScan scan = table.newScan();
-		TableScan filteredScan = scan.filter(Expressions.equal("level", "31"));
+		TableScan filteredScan = scan.filter(Expressions.equal("id", 10));
 		Schema projection = scan.schema();
 		Iterable<CombinedScanTask> tasks = filteredScan.planTasks();
 
 		for (CombinedScanTask task : tasks) {
 			for (FileScanTask file : task.files()) {
-				System.out.println(file.file().toString());
+				System.out.println("数据所在文件："+file.file().toString());
 			}
 		}
 
 		// 根据字段找每一行数据
 		CloseableIterable<Record> result = IcebergGenerics.read(table)
-				.where(Expressions.equal("level", "31"))
+				.where(Expressions.greaterThan("id", 10))
 				.build();
 		for (Record record : result) {
-			System.out.println(record.get(1));
+			System.out.println(record.toString());
 		}
 
 	}
