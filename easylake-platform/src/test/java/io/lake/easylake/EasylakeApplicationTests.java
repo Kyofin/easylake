e84eb81b27e1bd446f464d336dd662caa54a7c91
huzekang <1040080742@qq.com>
2022-07-19 14:59:44 +0800

    增加覆盖和replacePartition、append

--- easylake-platform/src/test/java/io/lake/easylake/EasylakeApplicationTests.java
+++ easylake-platform/src/test/java/io/lake/easylake/EasylakeApplicationTests.java
@@ -3,11 +3,16 @@ package io.lake.easylake;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.AppendFiles;
 import org.apache.iceberg.CombinedScanTask;
+import org.apache.iceberg.DeleteFile;
 import org.apache.iceberg.FileFormat;
+import org.apache.iceberg.FileMetadata;
 import org.apache.iceberg.FileScanTask;
+import org.apache.iceberg.OverwriteFiles;
 import org.apache.iceberg.PartitionKey;
 import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.ReplacePartitions;
 import org.apache.iceberg.RowDelta;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.Table;
@@ -209,7 +214,7 @@ class EasylakeApplicationTests {
 			final Record record = gRecord
 					.copy("id", i + 10,
 							"event_time", System.currentTimeMillis(),
-							"message", String.format("val-%d", i));
+							"message", String.format("新值-%d", i));
 			expected.add(record);
 
 			unpartitionedWriter.write(record);
@@ -218,13 +223,10 @@ class EasylakeApplicationTests {
 		System.out.println("新增文件数：" + result.dataFiles().length);
 		System.out.println("删除文件数：" + result.deleteFiles().length);
 		// 提交事务
-		RowDelta rowDelta = table.newRowDelta();
-		Arrays.stream(result.dataFiles()).forEach(dataFile -> rowDelta.addRows(dataFile));
-		Arrays.stream(result.deleteFiles()).forEach(dataFile -> rowDelta.addDeletes(dataFile));
+		final AppendFiles appendFiles = table.newAppend();
+		Arrays.stream(result.dataFiles()).forEach(dataFile -> appendFiles.appendFile(dataFile));
 
-		rowDelta.validateDeletedFiles()
-				.validateDataFilesExist(Lists.newArrayList(result.referencedDataFiles()))
-				.commit();
+		appendFiles.commit();
 	}
 
 	@Test
@@ -244,8 +246,11 @@ class EasylakeApplicationTests {
 		HadoopTables tables = new HadoopTables(conf);
 		// 不指定namespace和表名，直接指定路径
 		final String tableLocation = "file:///Volumes/Samsung_T5/opensource/easylake/data/iceberg_warehouse/iceberg_warehouse/part_tb3";
+		final ImmutableMap<String, String> pros = ImmutableMap.of(
+				TableProperties.FORMAT_VERSION, "2"
+		);
 		tables.dropTable(tableLocation);
-		Table table = tables.create(schema, spec, tableLocation);
+		Table table = tables.create(schema, spec, pros, tableLocation);
 
 		// 写数据
 		final FileFormat fileFormat = FileFormat.valueOf("PARQUET");
@@ -294,6 +299,164 @@ class EasylakeApplicationTests {
 
 	}
 
+	@Test
+	public void replace2PartitionTable() throws IOException {
+		Schema schema = new Schema(
+				Types.NestedField.required(1, "id", Types.IntegerType.get()),
+				Types.NestedField.required(2, "province", Types.StringType.get()),
+				Types.NestedField.required(3, "city", Types.StringType.get()),
+				Types.NestedField.required(4, "message", Types.StringType.get())
+		);
+		PartitionSpec spec = PartitionSpec.builderFor(schema)
+				.identity("province")
+				.identity("city")
+				.build();
+
+		Configuration conf = new Configuration();
+		HadoopTables tables = new HadoopTables(conf);
+		// 不指定namespace和表名，直接指定路径
+		final String tableLocation = "file:///Volumes/Samsung_T5/opensource/easylake/data/iceberg_warehouse/iceberg_warehouse/part_tb3";
+		Table table = tables.load(tableLocation);
+
+		// 写数据
+		final FileFormat fileFormat = FileFormat.valueOf("PARQUET");
+		FileAppenderFactory<Record> appenderFactory = new GenericAppenderFactory(table.schema(), table.spec(), null,
+				table.schema(), null);
+
+		OutputFileFactory fileFactory = OutputFileFactory.builderFor(table, 1, 1).format(fileFormat).build();
+
+		// 非分区表可以直接用 UnpartitionedWriter，分区表可以用PartitionedWriter
+		final PartitionedWriter partitionedWriter = new PartitionedWriter(spec, fileFormat, appenderFactory,
+				fileFactory, table.io(), 128 * 1024 * 1024) {
+			@Override
+			protected PartitionKey partition(Object row) {
+				final GenericRecord genericRecord = (GenericRecord) row;
+				final PartitionKey partitionKey = new PartitionKey(spec, schema);
+				partitionKey.partition(genericRecord);
+				return partitionKey;
+			}
+		};
+
+		final GenericRecord gRecord = GenericRecord.create(schema);
+
+		List<Record> expected = Lists.newArrayList();
+		for (int i = 0; i < 5; i++) {
+			final HashMap<String, Object> hashMap = Maps.newHashMap();
+			hashMap.put("id", i + 1);
+			hashMap.put("province", "gd");
+			hashMap.put("city", "shenzhen");
+			hashMap.put("message", String.format("msg-%d", i * 20));
+			final Record record = gRecord.copy(hashMap);
+			expected.add(record);
+
+			partitionedWriter.write(record);
+		}
+		WriteResult result = partitionedWriter.complete();
+		System.out.println("新增文件数：" + result.dataFiles().length);
+		System.out.println("删除文件数：" + result.deleteFiles().length);
+
+		//ReplacePartitions
+		final ReplacePartitions replacePartitions = table.newReplacePartitions();
+		Arrays.stream(result.dataFiles()).forEach(e->replacePartitions.addFile(e));
+		replacePartitions.commit();
+
+	}
+
+
+	@Test
+	public void replace2UnPartitionTable() throws IOException {
+		Schema schema = new Schema(
+				Types.NestedField.required(1, "id", Types.IntegerType.get()),
+				Types.NestedField.required(2, "event_time", Types.LongType.get()),
+				Types.NestedField.required(3, "message", Types.StringType.get())
+		);
+		Configuration conf = new Configuration();
+		HadoopTables tables = new HadoopTables(conf);
+		// 不指定namespace和表名，直接指定路径
+		final String tableLocation = "file:///Volumes/Samsung_T5/opensource/easylake/data/iceberg_warehouse/iceberg_warehouse/tb6";
+		Table table = tables.load( tableLocation);
+
+		// 写数据
+		final FileFormat fileFormat = FileFormat.valueOf("PARQUET");
+		FileAppenderFactory<Record> appenderFactory = new GenericAppenderFactory(table.schema(), table.spec(), null,
+				table.schema(), null);
+
+		OutputFileFactory fileFactory = OutputFileFactory.builderFor(table, 1, 1).format(fileFormat).build();
+
+		// 非分区表可以直接用 UnpartitionedWriter，分区表可以用PartitionedWriter
+		final MyTaskWriter taskWriter = new MyTaskWriter(table.spec(),
+				fileFormat,
+				appenderFactory,
+				fileFactory,
+				table.io(), 128 * 1024 * 1024);
+
+		final GenericRecord gRecord = GenericRecord.create(schema);
+
+		List<Record> expected = Lists.newArrayList();
+		for (int i = 0; i < 5; i++) {
+			final Record record = gRecord
+					.copy("id", i + 1, "event_time", System.currentTimeMillis(), "message", String.format("val-%d", i));
+			expected.add(record);
+
+			taskWriter.write(record);
+		}
+		WriteResult result = taskWriter.complete();
+		System.out.println("新增文件数：" + result.dataFiles().length);
+		System.out.println("删除文件数：" + result.deleteFiles().length);
+		// 提交事务
+		final ReplacePartitions replacePartitions = table.newReplacePartitions();
+		Arrays.stream(result.dataFiles()).forEach(dataFile -> replacePartitions.addFile(dataFile));
+		replacePartitions.commit();
+
+	}
+
+	@Test
+	public void overwrite2UnPartitionTable() throws IOException {
+		Schema schema = new Schema(
+				Types.NestedField.required(1, "id", Types.IntegerType.get()),
+				Types.NestedField.required(2, "event_time", Types.LongType.get()),
+				Types.NestedField.required(3, "message", Types.StringType.get())
+		);
+		Configuration conf = new Configuration();
+		HadoopTables tables = new HadoopTables(conf);
+		// 不指定namespace和表名，直接指定路径
+		final String tableLocation = "file:///Volumes/Samsung_T5/opensource/easylake/data/iceberg_warehouse/iceberg_warehouse/tb6";
+		Table table = tables.load( tableLocation);
+
+		// 写数据
+		final FileFormat fileFormat = FileFormat.valueOf("PARQUET");
+		FileAppenderFactory<Record> appenderFactory = new GenericAppenderFactory(table.schema(), table.spec(), null,
+				table.schema(), null);
+
+		OutputFileFactory fileFactory = OutputFileFactory.builderFor(table, 1, 1).format(fileFormat).build();
+
+		// 非分区表可以直接用 UnpartitionedWriter，分区表可以用PartitionedWriter
+		final MyTaskWriter taskWriter = new MyTaskWriter(table.spec(),
+				fileFormat,
+				appenderFactory,
+				fileFactory,
+				table.io(), 128 * 1024 * 1024);
+
+		final GenericRecord gRecord = GenericRecord.create(schema);
+
+		List<Record> expected = Lists.newArrayList();
+		for (int i = 0; i < 5; i++) {
+			final Record record = gRecord
+					.copy("id", i + 1, "event_time", System.currentTimeMillis(), "message", String.format("val-%d", i));
+			expected.add(record);
+
+			taskWriter.write(record);
+		}
+		WriteResult result = taskWriter.complete();
+		System.out.println("新增文件数：" + result.dataFiles().length);
+		System.out.println("删除文件数：" + result.deleteFiles().length);
+		// 提交事务
+		final OverwriteFiles overwriteFiles = table.newOverwrite();
+		overwriteFiles.overwriteByRowFilter(Expressions.alwaysTrue());
+		Arrays.stream(result.dataFiles()).forEach(dataFile -> overwriteFiles.addFile(dataFile));
+		overwriteFiles.commit();
+
+	}
 
 	@Test
 	public void readTable() {
